cf651981e9bc5408bd3c6d1a53a5111bbe8da33d
mwkho <martin.h@hotmail.ca>
2021-04-27 07:47:17 +0000

    Add validation to employee and store table

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
-
+  # store as a foreign key
+  belongs_to :store
+  validates :first_name, :last_name, :store, presence: true
+  validates :hourly_rate, numericality: {greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 2 }
+  validates :annual_revenue, numericality: {greater_than: 0, only_interger: true} 
+  validate :contain_men_or_women_apparel
+
+  def contain_men_or_women_apparel
+    if !(mens_apparel || womens_apparel)
+      errors.add(:womens_apparel, :mens_apparel, message: "The store needs to sell men or women apparrel")
+    end
+  end
 
 end
