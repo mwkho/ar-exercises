59751360f44275c33e1109b367e5142b1f2440c6
mwkho <martin.h@hotmail.ca>
2021-04-27 07:46:32 +0000

    Complete ex4 and 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "----- Total revuenue from all stores ---------"
+puts Store.sum(:annual_revenue)
+
+puts "----- Average annual revenue from all store is----------"
+puts Store.average(:annual_revenue)
+
+puts "-------Number of stores generating more than  $1M -----------"
+puts Store.where(["annual_revenue > ?", 1000000]).count
